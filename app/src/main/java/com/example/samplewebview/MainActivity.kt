17df57cf1006cc6654d9a133807f983192adc870
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-07-29 06:33:51 +0900

    Remove import

--- app/src/main/java/com/example/samplewebview/MainActivity.kt
+++ app/src/main/java/com/example/samplewebview/MainActivity.kt
@@ -8,10 +8,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.samplewebview.ui.theme.SampleWebViewTheme
 
